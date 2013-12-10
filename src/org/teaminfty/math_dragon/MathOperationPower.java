1ed93a12e426339e8efc16034797dd74fe784c78
Mark Kamsma <mark@markkamsma.nl>
2013-12-10 18:17:36 +0100

    Optimized the bounding box calculation for MathOperationPower Also made it so that it's always positioned right.
    Signed-off-by: Mark Kamsma <mark@markkamsma.nl>

--- src/org/teaminfty/math_dragon/MathOperationPower.java
+++ src/org/teaminfty/math_dragon/MathOperationPower.java
@@ -36,66 +36,62 @@ public class MathOperationPower extends MathBinaryOperation
 	@Override
 	public Rect[] getOperatorBoundingBoxes(int maxWidth, int maxHeight) 
 	{
-		// Powers don't have an operator sign
-		// You still have to set a bounding box, otherwise a nullpointexception will be thrown.
-		return new Rect[] {new Rect(0,0,0,0)};
+	    // Get the children sizes
+	    Rect[] sizes = getChildrenSize(maxWidth, maxHeight);
+	    
+		// Powers don't have a visible operator
+	    // However, they need an operator bounding box (for dropping other operations on them)
+		return new Rect[] {
+		        new Rect(0, 0, sizes[0].width(), sizes[1].height()),
+                new Rect(sizes[0].width(), sizes[1].height(), sizes[0].width() + sizes[1].width(), sizes[0].height() + sizes[1].height())
+	        };
 	}
 
+    /**
+     * Returns the sizes of the bounding of the children.
+     * 
+     * @param maxWidth
+     *        The maximum width the {@link MathObject} can have (can be {@link MathObject#NO_MAXIMUM})
+     * @param maxHeight
+     *        The maximum height the {@link MathObject} can have (can be {@link MathObject#NO_MAXIMUM})
+     * @return The size of the child bounding boxes
+     */
 	public Rect[] getChildrenSize(int maxWidth, int maxHeight)
 	{
-		// Get the bounding box both operands want to take
-        Rect leftSize = getChild(0) == null ? getRectBoundingBox(NO_MAXIMUM,
-                NO_MAXIMUM, EMPTY_CHILD_RATIO) : getChild(0).getBoundingBox(
-                NO_MAXIMUM, NO_MAXIMUM);
-        Rect rightSize = getChild(1) == null ? getRectBoundingBox(NO_MAXIMUM,
-        		NO_MAXIMUM, EMPTY_CHILD_RATIO) : getChild(1).getBoundingBox(
-                NO_MAXIMUM, NO_MAXIMUM);
+		// Get the sizes both operands want to take
+        Rect leftSize = getChild(0) == null ? getRectBoundingBox(NO_MAXIMUM, NO_MAXIMUM, EMPTY_CHILD_RATIO) : getChild(0).getBoundingBox(NO_MAXIMUM, NO_MAXIMUM);
+        Rect rightSize = getChild(1) == null ? getRectBoundingBox(NO_MAXIMUM, NO_MAXIMUM, EMPTY_CHILD_RATIO) : getChild(1).getBoundingBox(NO_MAXIMUM, NO_MAXIMUM);
         
-        // Multiply the exponent with a scaling factor so the exponent will be smaller
-        final double scaling = 0.5;
-        rightSize.bottom = (int) (rightSize.top + rightSize.height() * scaling);
-        rightSize.right = (int) (rightSize.left + rightSize.width() * scaling);
+        // Make sure the exponent is smaller than the base
+        rightSize.bottom = 2 * rightSize.bottom / 3;
+        rightSize.right = 2 * rightSize.right / 3;
         
-        // Check if the bounding boxes fit in the specified maxim, otherwise change them to fit       
-        if(	maxWidth != NO_MAXIMUM &&
-        	leftSize.width() + rightSize.width() > maxWidth)
-        {
-        	// Calculate the new width's of the bounding boxes
-        	final int leftWidthMax = leftSize.width() * maxWidth / (leftSize.width() + rightSize.width());
-        	final int rightWidthMax = rightSize.width() * maxWidth / (leftSize.width() + rightSize.width());
-        	
-        	// Make the new Bounding Boxes with the correct Size
-        	leftSize = getChild(0) == null ? getRectBoundingBox(leftWidthMax,
-                    leftSize.height(), EMPTY_CHILD_RATIO) : getChild(0).getBoundingBox(
-                    leftWidthMax, leftSize.height());
-            rightSize = getChild(1) == null ? getRectBoundingBox(rightWidthMax,
-            		rightSize.height(), EMPTY_CHILD_RATIO) : getChild(1).getBoundingBox(
-                    rightWidthMax, rightSize.height());
-        }
+        // If the boxes fit within the specified maximum, we're done
+        if((maxWidth == NO_MAXIMUM || leftSize.width() + rightSize.width() < maxWidth) && (maxHeight == NO_MAXIMUM || leftSize.height() + rightSize.height() < maxHeight))
+            return new Rect[] {leftSize, rightSize};
         
-        // Check if the Height of the power is in the bounding box
-        if(	maxHeight != NO_MAXIMUM &&
-            (maxHeight - leftSize.height()) / 2 + leftSize.height() + rightSize.height() > maxHeight)
-        {        	
-        	/* 	Calculate the new height's of the bounding boxes
-				The left side shouldn't scale, otherwise it would get out of size in a linear equation, it
-        		should, however, still fit in the bounding box. */
-        	final int leftHeightMax = (int) Math.min(maxHeight, leftSize.height());
-        	// The right side has to adjust to fit in the remaining space in the bounding box.
-            final int rightHeightMax = (int) Math.max(0, maxHeight - (maxHeight - leftHeightMax) / 2 - leftHeightMax);
-            
-            // Make the new Bounding Boxes with the correct Size
-            leftSize = getChild(0) == null ? getRectBoundingBox(leftSize.width(),
-                    leftHeightMax, EMPTY_CHILD_RATIO) : getChild(0).getBoundingBox(
-                    leftSize.width(), leftHeightMax);
-            rightSize = getChild(1) == null ? getRectBoundingBox(rightSize.width(),
-            		rightHeightMax, EMPTY_CHILD_RATIO) : getChild(1).getBoundingBox(
-            		rightSize.width(), rightHeightMax);
-        }
+        // Calculate the shrinking factor
+        final float widthFactor = maxWidth == NO_MAXIMUM ? 1.0f : ((float) maxWidth) / (leftSize.width() + rightSize.width());
+        final float heightFactor = maxHeight == NO_MAXIMUM ? 1.0f : ((float) maxHeight) / (leftSize.height() + rightSize.height());
+        final float factor = Math.min(widthFactor, heightFactor);
+        
+    	// Calculate the sizes
+    	leftSize.set(0, 0, (int) (leftSize.width() * factor), (int) (leftSize.height() * factor));
+    	rightSize.set(0, 0, (int) (rightSize.width() * factor), (int) (rightSize.height() * factor));
         
         // Return the Sizes
 		return new Rect[] {leftSize, rightSize};
 	}
+
+    @Override
+    public Rect getBoundingBox(int maxWidth, int maxHeight)
+    {
+        // Get the sizes
+        Rect[] sizes = getChildrenSize(maxWidth, maxHeight);
+        
+        // Return a bounding box, containing the bounding boxes of the children
+        return new Rect(0, 0, sizes[0].width() + sizes[1].width(), sizes[0].height() + sizes[1].height() * 2);
+    }
 	
 	@Override
 	public Rect getChildBoundingBox(int index, int maxWidth, int maxHeight)
@@ -108,15 +104,16 @@ public class MathOperationPower extends MathBinaryOperation
 		Rect[] childrenSize = getChildrenSize(maxWidth, maxHeight);
 		
 		// Move the bounding boxes to the correct position
-		childrenSize[0].offsetTo( 0, (maxHeight - childrenSize[0].height()) / 2);
-		childrenSize[1].offsetTo( childrenSize[0].width(), (int) (0.5 * (maxHeight - childrenSize[0].height()) - childrenSize[1].height()));
+		childrenSize[0].offsetTo(0, childrenSize[1].height());
+		childrenSize[1].offsetTo(childrenSize[0].width(), 0);
 		
+		// Return the right bounding box
 		return childrenSize[index];
 	}
 
 	@Override
 	public void draw(Canvas canvas, int maxWidth, int maxHeight) 
-	{		
+	{
 		// Only draw the children
 		drawLeft(canvas, getChildBoundingBox(0, maxWidth, maxHeight));
         drawRight(canvas, getChildBoundingBox(1, maxWidth, maxHeight));
