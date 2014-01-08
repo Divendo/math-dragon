b39826b3bbe44c3b0ebe5273fe60edb220c2b61f
Mark Kamsma <mark@markkamsma.nl>
2014-01-09 00:08:01 +0100

    Added tabs to the operation source drawer
    Signed-off-by: Mark Kamsma <mark@markkamsma.nl>

--- src/org/teaminfty/math_dragon/view/fragments/FragmentOperationsSource.java
+++ src/org/teaminfty/math_dragon/view/fragments/FragmentOperationsSource.java
@@ -17,6 +17,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ToggleButton;
 
 public class FragmentOperationsSource extends Fragment implements MathSourceView.DragStartedListener
 {
@@ -24,9 +25,10 @@ public class FragmentOperationsSource extends Fragment implements MathSourceView
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        View layout = inflater.inflate(R.layout.fragment_operations_source, container, false);
+        ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.fragment_operations_source, container, false);
         
         // Set the MathObjects for the MathSourceViews
+        // Operators
         setMathSourceObjectFor(layout, R.id.mathSourceAdd, new MathSourceBinaryOperationLinear(MathSourceBinaryOperationLinear.OperatorType.ADD));
         setMathSourceObjectFor(layout, R.id.mathSourceSubtract, new MathSourceBinaryOperationLinear(MathSourceBinaryOperationLinear.OperatorType.SUBTRACT));
         setMathSourceObjectFor(layout, R.id.mathSourceMultiply, new MathSourceBinaryOperationLinear(MathSourceBinaryOperationLinear.OperatorType.MULTIPLY));
@@ -35,10 +37,25 @@ public class FragmentOperationsSource extends Fragment implements MathSourceView
         setMathSourceObjectFor(layout, R.id.mathSourceRoot, new MathSourceOperationRoot());
         setMathSourceObjectFor(layout, R.id.mathSourceDerivative, new MathSourceOperationDerivative());
         setMathSourceObjectFor(layout, R.id.mathSourceIntegral, new MathSourceOperationIntegral());
+        // Functions
         setMathSourceObjectFor(layout, R.id.mathSourceSin, new MathSourceOperationFunction(MathOperationFunction.FunctionType.SIN));
         setMathSourceObjectFor(layout, R.id.mathSourceArcSin, new MathSourceOperationFunction(MathOperationFunction.FunctionType.ARCSIN));
-        setMathSourceObjectFor(layout, R.id.mathSourceSinh, new MathSourceOperationFunction(MathOperationFunction.FunctionType.SINH));
         setMathSourceObjectFor(layout, R.id.mathSourceCos, new MathSourceOperationFunction(MathOperationFunction.FunctionType.COS));
+        setMathSourceObjectFor(layout, R.id.mathSourceArcCos, new MathSourceOperationFunction(MathOperationFunction.FunctionType.ARCCOS));
+        setMathSourceObjectFor(layout, R.id.mathSourceTan, new MathSourceOperationFunction(MathOperationFunction.FunctionType.TAN));
+        setMathSourceObjectFor(layout, R.id.mathSourceArcTan, new MathSourceOperationFunction(MathOperationFunction.FunctionType.ARCTAN));
+        setMathSourceObjectFor(layout, R.id.mathSourceSinh, new MathSourceOperationFunction(MathOperationFunction.FunctionType.SINH));
+        setMathSourceObjectFor(layout, R.id.mathSourceCosh, new MathSourceOperationFunction(MathOperationFunction.FunctionType.COSH));
+        /* setMathSourceObjectFor(layout, R.id.mathSourceLog, ); */
+        setMathSourceObjectFor(layout, R.id.mathSourceLn, new MathSourceOperationFunction(MathOperationFunction.FunctionType.LN));
+        
+        // Get the tabs and set their OnClickListener and activate the right one
+        ToggleButton tabOperators = (ToggleButton) layout.findViewById(R.id.btn_tab_operators);
+        ToggleButton tabFunctions = (ToggleButton) layout.findViewById(R.id.btn_tab_funcs);
+        tabOperators.setChecked(true);
+        TabOnClickListener tabOnClickListener = new TabOnClickListener();
+        tabOperators.setOnClickListener(tabOnClickListener);
+        tabFunctions.setOnClickListener(tabOnClickListener);
         
         // Return the layout
         return layout;
@@ -72,5 +89,29 @@ public class FragmentOperationsSource extends Fragment implements MathSourceView
         if(onCloseMe != null)
             onCloseMe.closeMe();
     }
-
+    
+    /** The OnClickListener for the tab buttons */
+    private class TabOnClickListener implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View btn)
+        {
+            // Get the tabs and containers
+            ToggleButton tabOperators = (ToggleButton) getView().findViewById(R.id.btn_tab_operators);
+            ToggleButton tabFunctions = (ToggleButton) getView().findViewById(R.id.btn_tab_funcs);
+            ViewGroup containerOperators = (ViewGroup) getView().findViewById(R.id.operators_container);
+            ViewGroup containerFunctions = (ViewGroup) getView().findViewById(R.id.functions_container);
+            
+            // Determine which container is to be shown
+            final boolean showOperators = (btn.getId() == R.id.btn_tab_operators);
+            
+            // Activate the right tab (and deactivate the other)
+            tabOperators.setChecked(showOperators);
+            tabFunctions.setChecked(!showOperators);
+            
+            // Show/hide the containers
+            containerOperators.setVisibility(showOperators ? View.VISIBLE : View.INVISIBLE);
+            containerFunctions.setVisibility(showOperators ? View.INVISIBLE : View.VISIBLE);
+        }
+    }
 }
