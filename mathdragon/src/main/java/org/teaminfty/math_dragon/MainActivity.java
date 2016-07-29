724b9ff7afbf691d755527f1e3f6a735b9a0ce22
Mark Kamsma <mark@markkamsma.nl>
2016-07-29 18:25:38 +0200

    Replaced the Symja library with a newer (.aar) version.
    Signed-off-by: Mark Kamsma <mark@markkamsma.nl>

--- mathdragon/src/main/java/org/teaminfty/math_dragon/MainActivity.java
+++ mathdragon/src/main/java/org/teaminfty/math_dragon/MainActivity.java
@@ -2,7 +2,6 @@ package org.teaminfty.math_dragon;
 
 import org.matheclipse.core.eval.EvalEngine;
 import org.matheclipse.core.expression.F;
-import org.teaminfty.math_dragon.R;
 import org.teaminfty.math_dragon.model.Database;
 import org.teaminfty.math_dragon.model.EvalHelper;
 import org.teaminfty.math_dragon.view.TypefaceHolder;
